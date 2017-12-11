da3b27fa9edbab7e137b64b7545296f641640ef2
micogian <giannidose@tin.it>
2017-12-11 21:14:47 +0100

    Add files via upload

---
+++ includes/lastpictures_var.php
@@ -0,0 +1,12 @@
+<?php
+global $list_attach;
+global $list_topics;
+global $list_posts;
+global $list_views;
+
+/** LISTA DEI FORUM DA ELABORARE */
+$list_attach	= '41,42,102,109,121'; // Ultime immagini
+$list_topics	= '41,42,102,109,121'; // Ultimi topics
+$list_posts		= '41,42,102,109,121'; // Ultimi topics
+$list_views		= '41,42,102,109,121'; // Topics più visti
+?>
