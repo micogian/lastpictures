74ded8922fa4b54dd70afcb295093636dc7387da
micogian <giannidose@tin.it>
2018-01-12 17:47:46 +0100

    Update lastpictures_var.php

--- includes/lastpictures_var.php
+++ includes/lastpictures_var.php
@@ -7,6 +7,6 @@ global $list_views;
 /** LISTA DEI FORUM DA ELABORARE */
 $list_attach	= '2,6,7,8,9,10,11'; // Ultime immagini
 $list_topics	= '2,6,7,8,9,10,11'; // Ultimi topics
-$list_posts		= '2,6,7,8,9,10,11'; // Ultimi posts
-$list_views		= '2,6,7,8,9,10,11'; // Topics più visti
+$list_posts	= '2,6,7,8,9,10,11'; // Ultimi posts
+$list_views	= '2,6,7,8,9,10,11'; // Topics più visti
 ?>
